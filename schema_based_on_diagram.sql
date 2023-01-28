93f3622aecf015a38ae6d1f3a9d12917cadcb3de
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-01-28 18:14:12 +0100

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,44 +1,58 @@
--- Create table animals
-CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight FLOAT NOT NULL,
-    species_id INTEGER REFERENCES species(id)
+CREATE DATABASE clinic;
+
+CREATE TABLE patients (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    date_of_birth DATE NOT NULL
 );
 
--- Create Table species
-CREATE TABLE species (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    vaccineRequired BOOLEAN NOT NULL
+CREATE TABLE medical_histories (
+    id SERIAL NOT NULL PRIMARY KEY,
+    addmitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL,
+    status VARCHAR(250),
+    constraint fK_patient_id foreign key(fK_patient_id) references 
+     patients(id)
+);
+
+CREATE TABLE treatments (
+    id SERIAL NOT NULL PRIMARY KEY,
+    type VARCHAR(250) NOT NULL,
+    name VARCHAR(250)
+);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY NOT NULL,
+    total-amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_histories_id INT NOT NULL,
+    constraint fK_medical_histories_id foreign key(fk_medical_histories_id) references medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+   id SERIAL PRIMARY KEY,
+   unit_price DECIMAL NOT NULL,
+   quantity INT NOT NULL,
+   total_price DECIMAL NOT NULL,
+   invoice_id INT,
+   treatments_id INT,
+   constraint fk_invoice_id foreign key(fk_invoice_id) references invoices(id),
+   constraint fk_treatments_id foreign key(fk_treatments_id) references treatments(id),
+);
+
+CREATE TABLE medical_histories_treatment (
+    medical_histories_id INT PRIMARY KEY NOT NULL,
+    treatments_id INT PRIMARY KEY NOT NULL,
+    constraint medical_histories_id foreign key(medical_histories_id) references 
+    medical_histories(id), 
+    constraint treatments_id foreign key(treatments_id) references treatments(id)
 );
 
 
--- Create table owners
-CREATE TABLE owners (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    animal_id INTEGER REFERENCES animals(id)
-);
-
--- Create table employee
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL,
-    designation VARCHAR(255) NOT NULL,
-    doj DATE NOT NULL
-);
-
--- Create table visits
-CREATE TABLE visits (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    animal_id INTEGER REFERENCES animals(id),
-    employee_id INTEGER REFERENCES employee(id),
-    owner_id INTEGER REFERENCES owners(id)
-);
+CREATE INDEX ON medical_histories(patient_id);
+CREATE INDEX ON invoices(medical_histories_id);
+CREATE INDEX ON invoice_items(invoice_id);
+CREATE INDEX ON invoice_items(treatments_id);
+CREATE INDEX ON medical_histories_treatment(medical_histories_id);
+CREATE INDEX ON medical_histories_treatment(treatments_id);
